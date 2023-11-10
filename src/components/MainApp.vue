5cd5287dd47a6c964a892d7df2e817321abe4b5d
klelia <clelia.fradella@gmail.com>
2023-11-10 13:13:39 +0100

    Card Component

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,15 +1,33 @@
 <template>
     <main>
         <section class="container">
-            <h1>Content goes here</h1>
+            <div class="row gy-2">
+                <div class="col-12 col-md-4 col-lg-3" v-for="(pasta, index) in pastas" :key="index">
+                    <CardComponent :image="pasta.src" :title="pasta.titolo" />
+                </div>
+
+            </div>
         </section>
+
 
     </main>
 </template>
 
 <script>
+import { products } from '../data/product.js';
+import CardComponent from './CardComponent.vue';
+
 export default {
     name: 'MainApp',
+    comnponents: {
+        CardComponent
+    },
+    data() {
+        return {
+            pastas: products
+        };
+    },
+    components: { CardComponent }
 }
 </script>
 
